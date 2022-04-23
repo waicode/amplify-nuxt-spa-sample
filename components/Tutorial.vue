4ce6520bb18c190a3773eed4ab8fb49b27f17a8d
waicode <waicode37@gmail.com>
2022-04-23 18:56:35 +0900

    fix: url typo

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -72,7 +72,7 @@
         </ul>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
           より詳しく知りたい方は<a
-            href="https://github.com/waicode/amplify-nuxt-spa-sampl"
+            href="https://github.com/waicode/amplify-nuxt-spa-sample"
             target="_blank"
             rel="nofollow noopener"
             class="button--doc text-green-500 hover:underline"
